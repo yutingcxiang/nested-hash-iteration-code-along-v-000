39090e6a97f8054b10db2d474413154131a12ff0
Christine Xiang <yutingcxiang@gmail.com>
2018-07-09 22:47:57 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,7 +16,7 @@ require 'pry'
   
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
-    contact_details_hash.each do |attribtue, data|
+    contact_details_hash.each do |attribute, data|
       binding.pry
     end
   end
